d78ae0a9b3054e8e23ba3ce0513d1807f4f2fd4b
martapienkosz <70956012+martapienkosz@users.noreply.github.com>
2022-10-14 13:17:40 -0400

    newOnes

---
+++ Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneSwitcher : MonoBehaviour
+{
+
+	public int currentScene;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		DontDestroyOnLoad(this.gameObject);
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.anyKey)
+		{
+			currentScene++;
+			if (currentScene == SceneManager.sceneCountInBuildSettings)
+			{
+				currentScene = 0;
+			}
+			SceneManager.LoadScene(currentScene);
+			
+		}
+
+	}
+}
